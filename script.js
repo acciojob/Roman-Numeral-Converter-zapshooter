e65516c75cade19ff582a683030d71f0fceeb560
zapshooter <92568875+zapshooter@users.noreply.github.com>
2025-08-12 22:17:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
 function convertToRoman(num) {
     if (num === 0) {
-        return ''; //
+        return '';
     }
-    const obj = {
-        0: ['M', 1000],
-        1: ['CM', 900],
-        2: ['D', 500],
-        3: ['CD', 400],
-        4: ['C', 100],
-        5: ['XC', 90],
-        6: ['L', 50],
-        7: ['XL', 40],
-        8: ['X', 10],
-        9: ['IX', 9],
-        10: ['V', 5],
-        11: ['IV', 4],
-        12: ['I', 1]
-    };
+    const romanNumerals = [
+        ['M', 1000],
+        ['CM', 900],
+        ['D', 500],
+        ['CD', 400],
+        ['C', 100],
+        ['XC', 90],
+        ['L', 50],
+        ['XL', 40],
+        ['X', 10],
+        ['IX', 9],
+        ['V', 5],
+        ['IV', 4],
+        ['I', 1]
+    ];
     let roman = '';
-    for (let key in obj) {
-        const [symbol, value] = obj[key];
+    for (let [symbol, value] of romanNumerals) {
         while (num >= value) {
             roman += symbol;
             num -= value;
